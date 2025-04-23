933482c9b5d2807c9638eb6ea489a8baf9586fa1
akashagalave <akashagalaveaaa1@gmail.com>
2025-04-23 18:56:34 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLops End To End Project For Food Delivery Time Prediction.',
+    author='akash agalave',
+    license='',
+)
